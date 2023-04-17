c90304328472df72db3e6927792e868c9b039e48
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2023-04-17 11:14:35 +0200

    pushed current staircase

--- src/main/java/TicTacToe/Shapes/TicTacToeStairs.java
+++ src/main/java/TicTacToe/Shapes/TicTacToeStairs.java
@@ -12,15 +12,57 @@ public class TicTacToeStairs {
     public Geometry3D GeneratePlate(JavaCSG csg) {
         Geometry3D product = null;
 
-        Geometry3D cyl = csg.cylinder3D(10, 20, 32, true);
-        Geometry3D square = csg.box3D(90, 90, 1, true);
-        product = csg.difference3D(square, cyl);
-        Geometry3D inter = csg.intersection3D(square, cyl );
-        inter = csg.translate3DZ(10).transform(inter);
-//        product = csg.union3D(diff, inter);
-//		csg.view(diff);
-//        csg.view(union);
-//		csg.view(inter);
+        JavaCSG csg = JavaCSGFactory.createDefault();
+
+        Geometry3D plate = csg.box3D(60, 60, 10, false);
+        plate = csg.translate3D(30, 30,0).transform(plate);
+        Geometry3D plate2 = csg.box3D(25, 25, 15, false);
+
+        plate2 = csg.translate3D(25, 25,0).transform(plate2);
+
+        var plates1 = csg.box3D(60,20,15, false);
+        plates1 = csg.translate3D(30, 10,0).transform(plates1);
+        var plates2 = csg.box3D(40,20,15, false);
+        plates2 = csg.translate3D(40, 30,0).transform(plates2);
+        var plates3 = csg.box3D(20,20,15, false);
+        plates3 = csg.translate3D(50, 50,0).transform(plates3);
+
+        var unionPlate2 = csg.union3D(plates1, plates2, plates3);
+
+        var upperPlate = csg.box3D(40, 20, 20, false);
+        upperPlate = csg.translate3D(40, 10,0).transform(upperPlate);
+
+        var upperPlate2 = csg.box3D(20, 20, 20, false);
+        upperPlate2 = csg.translate3D(50, 30,0).transform(upperPlate2);
+
+        var upperUnion = csg.union3D(upperPlate, upperPlate2);
+
+        var uppestPlate = csg.box3D(20, 20, 25, false);
+        uppestPlate = csg.translate3D(50, 10,0).transform(uppestPlate);
+
+        var lowerPlate = csg.box3D(20, 20, 15, false);
+        lowerPlate = csg.translate3D(10, 50,5).transform(lowerPlate);
+
+
+
+        var removeVol = csg.box3D(18, 18, 21, false);
+        removeVol = csg.translate3D(50, 30,8).transform(removeVol);
+
+        var union = csg.union3D(plate, unionPlate2,upperUnion, uppestPlate);
+
+
+        // removevols
+
+        var lowestPlate = csg.box3D(18, 18, 5, false);
+        lowestPlate = csg.translate3D(10, 50,5).transform(lowestPlate);
+
+        var newUnion = csg.difference3D(union, lowestPlate);
+        newUnion = csg.difference3D(union, lowerPlate);
+
+
+        removeVol = csg.translate3D(30,10,5).transform(removeVol);
+        newUnion = csg.difference3D(newUnion, removeVol);
+        product = newUnion;
 
 
         return product;
